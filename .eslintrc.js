8e0d0e1940c459c7daef0ade5516758ff4817665
Denys Rudyk <rudykdenys@gmail.com>
2024-09-18 14:13:38 +0300

    chore: sync eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ["@react-native", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      1,
+      {
+        arrowParens: "always",
+        bracketSameLine: true,
+        bracketSpacing: false,
+        singleQuote: false,
+        trailingComma: "all",
+      },
+    ],
+  },
 };
