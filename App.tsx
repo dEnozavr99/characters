dec1f70f998c51d07fa7d19ee8c96f33d888ee03
Denys Rudyk <rudykdenys@gmail.com>
2024-10-11 12:44:12 +0300

    feat: update app file with provider and main screen

--- App.tsx
+++ App.tsx
@@ -6,28 +6,18 @@
  */
 
 import React from "react";
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
 
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { MQTTProvider } from "./src/providers/mqtt";
 
-import MQTTScreen from "./src/mqtt/MQTTScreen";
+import { MainScreen } from "./src/screens";
+
+global.Buffer = require("buffer").Buffer;
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? "light-content" : "dark-content"}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-
-      <MQTTScreen />
-    </SafeAreaView>
+    <MQTTProvider>
+      <MainScreen />
+    </MQTTProvider>
   );
 }
 
