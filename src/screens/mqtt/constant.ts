e3f026dac0b0810f8cc6fbe76e7c73105abc81b4
Denys Rudyk <rudykdenys@gmail.com>
2024-10-11 16:19:36 +0300

    feat: check for test broker

--- src/screens/mqtt/constant.ts
+++ src/screens/mqtt/constant.ts
@@ -1,23 +1,24 @@
 import { ConnectionOptions } from "@ko-developerhong/react-native-mqtt";
 
-const MQTT_PORT = "1883";
+// const MQTT_PORT = "1883";
 
-const BROKER = `mqtt://perfect-politician.cloudmqtt.com:${MQTT_PORT}`;
+// const BROKER = `mqtt://perfect-politician.cloudmqtt.com:${MQTT_PORT}`;
+const BROKER = `test.mosquitto.org`;
 
 const CLIENT_ID = "react_native_client";
 
-const USERNAME = "gelphadi";
-const PASSWORD = "oYMgWJETz_0N";
+// const USERNAME = "gelphadi";
+// const PASSWORD = "oYMgWJETz_0N";
 
 const CONNECTION_OPTIONS: ConnectionOptions = {
   clientId: CLIENT_ID,
-  username: USERNAME,
-  password: PASSWORD,
+  // username: USERNAME,
+  // password: PASSWORD,
+  // keepAlive: 60,
+  // autoReconnect: true,
   // protocol: "mqtt",
   // allowUntrustedCA: true,
-  // keepAlive: 60,
   // maxInFlightMessages: 5,
-  // autoReconnect: false,
   // cleanSession: true,
   // timeout: 60,
   // tls: {
