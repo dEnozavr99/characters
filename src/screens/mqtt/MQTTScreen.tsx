31025a5fd4aa7486956f0fd82cb5db0ad0646847
Denys Rudyk <rudykdenys@gmail.com>
2024-10-11 15:47:52 +0300

    feat: update mqtt screen

--- src/screens/mqtt/MQTTScreen.tsx
+++ src/screens/mqtt/MQTTScreen.tsx
@@ -1,20 +1,64 @@
 import { Button, View, Text, StyleSheet } from "react-native";
 import React from "react";
 
+import { useMQTTConnection } from "./useMQTTConnection";
+
 const MQTTScreen = () => {
+  const {
+    isClientConnected,
+    currentData,
+    errorMessage,
+    handleConnect,
+    handleDisconnect,
+    handleSubscribe,
+    handlePublish,
+  } = useMQTTConnection();
+
+  const statusColor = isClientConnected ? "green" : "red";
+
   return (
-    <View style={style.container}>
-      <Text>MQTTScreen</Text>
-      <Button title="Publish" />
+    <View style={styles.container}>
+      <View style={styles.captionsContainer}>
+        <Text
+          style={{ color: statusColor }}
+        >{`Client is: ${isClientConnected ? "connected" : "disconnected"}`}</Text>
+        <Text>{`Topic: ${currentData?.topic} | Message: ${currentData?.message}`}</Text>
+        <Text>{`Error: ${errorMessage}`}</Text>
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.buttonsContainer}>
+          <Button color="green" title="Connect" onPress={handleConnect} />
+          <Button color="red" title="Disconnect" onPress={handleDisconnect} />
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button title="Subscribe" onPress={handleSubscribe} />
+          <Button title="Publish" onPress={handlePublish} />
+        </View>
+      </View>
     </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
+    padding: 32,
+  },
+  captionsContainer: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    gap: 32,
+  },
+  footerContainer: {
+    flex: 1,
+    gap: 32,
+    justifyContent: "space-evenly",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
 
