6fcb59cf0eac5c2f7f21917acb6ea77215a8f2d0
Denys Rudyk <rudykdenys@gmail.com>
2024-10-11 13:32:11 +0300

    feat: move mqtt screen

--- src/screens/MQTTScreen.tsx
+++ src/screens/MQTTScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { Button, View, Text } from "react-native";
 import React from "react";
 
 const MQTTScreen = () => {
   return (
     <View>
       <Text>MQTTScreen</Text>
+      <Button title="Publish" />
     </View>
   );
 };
