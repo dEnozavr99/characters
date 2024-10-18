541e7cfe553172538b60b10d6351f789d9ad8549
Denys Rudyk <rudykdenys@gmail.com>
2024-10-18 18:05:52 +0300

    feat: add test progress

--- src/screens/mqtt/MQTTScreen.tsx
+++ src/screens/mqtt/MQTTScreen.tsx
@@ -1,5 +1,7 @@
-import { Button, View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { Button, View, Text, StyleSheet, Easing } from "react-native";
+import React, { useRef } from "react";
+
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 import { useMQTTConnection } from "./useMQTTConnection";
 
@@ -17,6 +19,28 @@ const MQTTScreen = () => {
 
   const statusColor = isClientConnected ? "green" : "red";
 
+  const progressRef = useRef<AnimatedCircularProgress>(null);
+
+  const handleForvardProgress = () => {
+    const { current } = progressRef;
+
+    if (!current) {
+      return;
+    }
+
+    current.animate(100, 3000, Easing.linear);
+  };
+
+  const handleBackwardProgress = () => {
+    const { current } = progressRef;
+
+    if (!current) {
+      return;
+    }
+
+    current.animate(0, 3000, Easing.linear);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.captionsContainer}>
@@ -26,6 +50,47 @@ const MQTTScreen = () => {
         <Text>{`Topic: ${currentData?.topic} | Message: ${currentData?.message}`}</Text>
         <Text>{`Error: ${errorMessage}`}</Text>
       </View>
+
+      <View style={{ alignItems: "center" }}>
+        <AnimatedCircularProgress
+          ref={progressRef}
+          size={150}
+          width={5}
+          backgroundWidth={15}
+          padding={10}
+          fill={0}
+          tintColor="turquoise"
+          tintColorSecondary="#c93103"
+          lineCap="round"
+          // renderCap={({ center }) => (
+          //   <Circle cx={center.x} cy={center.y} r="10" fill="cyan" />
+          // )}
+          onAnimationComplete={() => {
+            console.log("onAnimationComplete");
+          }}
+          backgroundColor="#211eaa"
+          arcSweepAngle={300}
+          rotation={210}
+        >
+          {(fill) => <Text>{Math.round(fill)}</Text>}
+        </AnimatedCircularProgress>
+      </View>
+
+      <View style={styles.footerContainer}>
+        <View style={styles.buttonsContainer}>
+          <Button
+            color="green"
+            title="Forward animation"
+            onPress={handleForvardProgress}
+          />
+          <Button
+            color="red"
+            title="Backward animation"
+            onPress={handleBackwardProgress}
+          />
+        </View>
+      </View>
+
       <View style={styles.footerContainer}>
         <View style={styles.buttonsContainer}>
           <Button color="green" title="Connect" onPress={handleConnect} />
