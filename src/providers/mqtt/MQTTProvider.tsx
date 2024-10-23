af3807821a08fd41c1ddf2e0135b8aea7fb583d6
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 19:46:19 +0300

    feat: update mqtt provider

--- src/providers/mqtt/MQTTProvider.tsx
+++ src/providers/mqtt/MQTTProvider.tsx
@@ -4,40 +4,76 @@ import MqttClient, {
   ClientEvent,
   ConnectEventHandler,
   DisconnectEventHandler,
+  ErrorEventHandler,
+  MessageEventHandler,
 } from "@ko-developerhong/react-native-mqtt";
 
 import { MQTTContext } from "../../contexts/mqtt";
 
+import type { MessageData } from "../../contexts";
+
 export const MQTTProvider = ({ children }: PropsWithChildren) => {
   const [clientNext] = useState<typeof MqttClient>(MqttClient);
   const [isClientConnected, setIsClientConnected] = useState(false);
+  const [currentData, setCurrentData] = useState<MessageData | null>(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    const connectEventHandler: ConnectEventHandler = (reconnect) => {
-      console.log("IS RECONNECT", reconnect);
+    try {
+      const connectEventHandler: ConnectEventHandler = (reconnect) => {
+        console.log("IS RECONNECT", reconnect);
 
-      setIsClientConnected(true);
-    };
+        setIsClientConnected(true);
+      };
 
-    clientNext.on(ClientEvent.Connect, connectEventHandler);
+      clientNext.on(ClientEvent.Connect, connectEventHandler);
 
-    const disconnectEventHandler: DisconnectEventHandler = (cause) => {
-      console.log("DISCONNECT CAUSE", cause);
+      const disconnectEventHandler: DisconnectEventHandler = (cause) => {
+        console.log("DISCONNECT CAUSE", cause);
 
-      setIsClientConnected(false);
-    };
+        setIsClientConnected(false);
+      };
 
-    clientNext.on(ClientEvent.Disconnect, disconnectEventHandler);
+      clientNext.on(ClientEvent.Disconnect, disconnectEventHandler);
 
-    return () => {
-      clientNext.off(ClientEvent.Connect, connectEventHandler);
-      clientNext.off(ClientEvent.Disconnect, disconnectEventHandler);
-    };
+      const messageEventHandler: MessageEventHandler = (topic, message) => {
+        const newData = { topic, message: message.toString() };
+
+        console.log("TOPIC", newData.topic);
+        console.log("MESSAGE", newData.message);
+
+        setCurrentData(newData);
+      };
+
+      clientNext.on(ClientEvent.Message, messageEventHandler);
+
+      const errorEventHandler: ErrorEventHandler = (error) => {
+        console.log("ERROR", error);
+
+        setErrorMessage(error);
+      };
+
+      clientNext.on(ClientEvent.Error, errorEventHandler);
+
+      return () => {
+        clientNext.off(ClientEvent.Connect, connectEventHandler);
+        clientNext.off(ClientEvent.Disconnect, disconnectEventHandler);
+        clientNext.off(ClientEvent.Message, messageEventHandler);
+        clientNext.off(ClientEvent.Error, errorEventHandler);
+      };
+    } catch (error) {
+      console.error("initialize mqtt error" + error);
+    }
   });
 
   return (
     <MQTTContext.Provider
-      value={{ client: clientNext, isConnected: isClientConnected }}
+      value={{
+        client: clientNext,
+        isConnected: isClientConnected,
+        data: currentData,
+        errorMessage,
+      }}
     >
       {children}
     </MQTTContext.Provider>
