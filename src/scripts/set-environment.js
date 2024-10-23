062b6321162b70c715e277de68133fee647e897e
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 17:08:16 +0300

    feat: remove env changing script

--- src/scripts/set-environment.js
+++
@@ -1,8 +0,0 @@
-#!/bin/node
-
-const fs = require("fs");
-
-const targetEnvironment = process.env[2];
-const envFileContent = require(`../../environments/.env.${targetEnvironment}`);
-
-fs.writeFileSync(".env", envFileContent);
