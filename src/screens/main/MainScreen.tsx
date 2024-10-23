ce905f20297fac36ee3e6603cfc7cbb6b5bb9d5c
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 19:47:16 +0300

    feat: update main screen

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
-
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import MQTTScreen from "../mqtt/MQTTScreen";
+import { CircularProgress } from "../../components";
 
 const MainScreen = () => {
+  const [animationValue, setAnimationValue] = useState(0);
+
+  const handleIncreaseValue = () => {
+    setAnimationValue((prevValue) => prevValue + 10);
+  };
+
+  const handleDecreaseValue = () => {
+    setAnimationValue((prevValue) => prevValue - 10);
+  };
+
   const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
@@ -21,9 +37,54 @@ const MainScreen = () => {
         backgroundColor={backgroundStyle.backgroundColor}
       />
 
-      <MQTTScreen />
+      <View style={styles.container}>
+        <View style={styles.progressContainer}>
+          <CircularProgress progressValue={animationValue} />
+        </View>
+
+        <View style={styles.footerContainer}>
+          <View style={styles.buttonsContainer}>
+            <Button
+              color="green"
+              title="Increase value"
+              onPress={handleIncreaseValue}
+            />
+            <Button
+              color="red"
+              title="Decrease value"
+              onPress={handleDecreaseValue}
+            />
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 32,
+  },
+  captionsContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 32,
+  },
+  footerContainer: {
+    flex: 1,
+    gap: 32,
+    justifyContent: "space-evenly",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  progressContainer: {
+    alignItems: "center",
+  },
+});
 
 export default MainScreen;
