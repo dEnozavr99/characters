036002e94a16931e31e958c41f38403be96dcee7
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 19:13:15 +0300

    feat: update root tab navigator

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,17 +1,65 @@
 import React from "react";
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import { ParamListBase, RouteProp } from "@react-navigation/native";
 
 import DashboardStackNavigator from "./DashboardStackNavigator";
 import SettingsStackNavigator from "./SettingsStackNavigator";
 
+import { TabRoutes } from "./constants";
+
 const Tab = createBottomTabNavigator();
 
+const IconsByRouteName: {
+  [key in TabRoutes]: string;
+} = {
+  [TabRoutes.DashboardTab]: "home",
+  [TabRoutes.SettingsTab]: "settings",
+};
+
+type TabIconProps = {
+  routeName: TabRoutes;
+  focused: boolean;
+  color: string;
+  size: number;
+};
+
+const renderTabIcon = ({ routeName, focused, color, size }: TabIconProps) => {
+  const iconName = `${IconsByRouteName[routeName]}${focused ? "" : "-outline"}`;
+  return <Ionicons name={iconName} size={size} color={color} />;
+};
+
 const RootNavigator = () => {
+  const screenOptionsSelector = ({
+    route,
+  }: {
+    route: RouteProp<ParamListBase>;
+    navigation: any;
+  }): BottomTabNavigationOptions => ({
+    headerShown: false,
+    tabBarIcon: ({ focused, color, size }) =>
+      renderTabIcon({
+        routeName: route.name as TabRoutes,
+        focused,
+        color,
+        size,
+      }),
+  });
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Dashboard Stack" component={DashboardStackNavigator} />
-      <Tab.Screen name="Settings Stack" component={SettingsStackNavigator} />
+    <Tab.Navigator screenOptions={screenOptionsSelector}>
+      <Tab.Screen
+        name={TabRoutes.DashboardTab}
+        component={DashboardStackNavigator}
+      />
+      <Tab.Screen
+        name={TabRoutes.SettingsTab}
+        component={SettingsStackNavigator}
+      />
     </Tab.Navigator>
   );
 };
