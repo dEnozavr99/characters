2ba9df1bb4da9536aef0da68a3e74cf4056588c9
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 12:20:13 +0300

    feat: update root navigator

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { MainScreen } from "../screens";
+import DashboardStackNavigator from "./DashboardStackNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +12,11 @@ const AnalyticsScreen = () => <Text>Analytics screen</Text>;
 const RootNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={MainScreen} />
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardStackNavigator}
+        options={{ headerShown: false }}
+      />
       <Tab.Screen name="Analytics" component={AnalyticsScreen} />
     </Tab.Navigator>
   );
