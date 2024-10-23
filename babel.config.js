7d767c1a3407f25ac375e430bdf4d95d6623b311
Denys Rudyk <rudykdenys@gmail.com>
2024-10-23 16:41:15 +0300

    feat: update config for getting the env type

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: [["module:react-native-dotenv"]],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ],
 };
